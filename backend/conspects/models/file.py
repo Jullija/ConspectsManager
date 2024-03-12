53c5089d49197a619af417bc7e4f14623774ae0c
Dominik Adamczyk <dominik.adamczyk1000@gmail.com>
2024-03-12 20:08:58 +0100

    changed name of file to be arbitrary

--- backend/conspects/models/file.py
+++ backend/conspects/models/file.py
@@ -3,17 +3,10 @@ from django.db import models
 
 
 class File(TimeStampedModel):
-    class ExtensionChoices(models.TextChoices):
-        TXT = "txt", "TXT"
-        PDF = "pdf", "PDF"
-        MD = "md", "markdown"
-        # TODO add more supported extensions
-
     name = models.CharField(max_length=255,
                             blank=False,
                             null=False)
     file_type = models.CharField(max_length=255,
-                                 choices=ExtensionChoices.choices,
                                  blank=False,
                                  null=False)
     content = models.BinaryField()

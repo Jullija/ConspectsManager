d3cd59edccbb3adbd6a770def68c30a04e713069
Anna Cichocka <115690306+AnCichocka@users.noreply.github.com>
2024-03-19 21:49:57 +0100

    [IOD-43] subject screen without backend added (#12)
    * subject styled
    
    * buttons font color and link to navigate changed
    
    ---------
    
    Co-authored-by: Anna Cichocka <410209@student.agh.edu.pl>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { RouterProvider } from 'react-router-dom';
-import './App.css';
 import routes from './router/routes';
 
 function App() {
--- frontend/src/screens/Subject/Subject.tsx
+++ frontend/src/screens/Subject/Subject.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Edition } from '../../utils/types';
 import { getEditions } from '../../api/editions';
 import EditionCard from './EditionCard';
 import { pathGenerator } from '../../router/paths';
+import { titleFontSize } from '../../utils/sizes';
+import { Button } from 'semantic-ui-react';
+import { colors } from '../../utils/colors';
 
 const Subject = () => {
   const params = useParams();
   const subjectId = Number(params.subjectId);
+  const navigate = useNavigate();
 
   const [editions, setEditions] = useState<Edition[]>();
 
@@ -19,16 +23,31 @@ const Subject = () => {
     <div
       style={{
         display: 'flex',
-        flexDirection: 'column',
-        rowGap: 20
-      }}
-    >
-      <p>subject subjectId: {subjectId} </p>
-      <Link to={pathGenerator.AddEdition(subjectId)}>
-        <button>add edition</button>
-      </Link>
+        flexDirection: 'column'
+      }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          margin: '24px 20px'
+        }}>
+        <div style={{ fontSize: titleFontSize }}>subject subjectId: {subjectId}</div>
+
+        <Button
+          style={{ backgroundColor: colors.blue, color: colors.white }}
+          onClick={() => navigate(pathGenerator.AddEdition(subjectId))}>
+          add edition
+        </Button>
+      </div>
+
       {editions?.map((edition, index) => (
-        <EditionCard key={index} edition={edition} subjectId={subjectId} />
+        <EditionCard
+          key={index}
+          edition={edition}
+          subjectId={subjectId}
+          withBottomBorder={index !== editions.length - 1}
+        />
       ))}
     </div>
   );

4ac191771c0e1186cf94665be38b5f5ec96ef268
Jullija <juleczek2002@gmail.com>
2024-03-24 16:57:53 +0100

    page not found screen

--- frontend/src/screens/PageNotFound/PageNotFound.tsx
+++ frontend/src/screens/PageNotFound/PageNotFound.tsx
@@ -3,7 +3,7 @@ import { colors } from "../../utils/colors";
 const PageNotFound = () => {
   return(
     <div style={{display: "flex", flexDirection:"column", justifyContent: "center", alignItems:"center"}}>
-      <h1 style={{color: colors.darkblue}}>Page. Not. Found. </h1>
+      <h1 style={{color: colors.darkblue}}>Page. Not. Found.</h1>
       <img src="https://tenor.com/en-GB/view/pulp-fiction-john-travolta-lost-where-wtf-gif-10251428.gif" style={{width: "700px", height:"700px"}}></img>
     </div>
 

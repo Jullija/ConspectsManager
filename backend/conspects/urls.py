e411455b4e49d3e39f048ade72482376f5710341
Szymon Nowak-Trzos <40152296+Szyntos@users.noreply.github.com>
2024-03-24 21:48:36 +0100

    IOD-24 Fixed Links (#20)
    * Added CRUD for files and handling markdown files
    
    * Changed the code to fit the changed models
    
    * Fixed Links
    
    * Removed used migrations

--- backend/conspects/urls.py
+++ backend/conspects/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 
-from conspects.views import RetrieveCreateCourseView, EditionListCreateAPIView, FolderCreateAPIView
+from conspects.views import RetrieveCreateCourseView, EditionListCreateAPIView, FolderCreateAPIView, FilesViewSet
 
 app_name = "conspects"
 
 router = routers.SimpleRouter(trailing_slash=False)
+router.register(r'files', FilesViewSet)
 
 # router.register(r"course", RetrieveCreateCourseView, basename="course")
 
@@ -13,4 +14,5 @@ urlpatterns = router.urls + [
     path('courses/', RetrieveCreateCourseView.as_view()),
     path('courses/<int:courseId>/editions/', EditionListCreateAPIView.as_view()),
     path('folders/', FolderCreateAPIView.as_view()),
+    path('', include(router.urls)),
 ]

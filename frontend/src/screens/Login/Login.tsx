7347d0a83e501e155eb346c090c7b715646c5228
Szymon Nowak-Trzos <szymtos@gmail.com>
2024-04-24 00:44:49 +0200

    Added Logout

--- frontend/src/screens/Login/Login.tsx
+++ frontend/src/screens/Login/Login.tsx
@@ -3,51 +3,16 @@ import { pathGenerator } from '../../router/paths';
 import { Button, Dropdown, DropdownProps } from 'semantic-ui-react'
 import { titleFontSize } from '../../utils/sizes';
 import { colors } from '../../utils/colors';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import app from '../../firebase/firebase-config';
 import { axiosClient } from '../../api/axiosClient';
 import axios from 'axios';
-
+import { useAuth } from '../../context/AuthContext';
 
 const Login = () => {
-
-
-
-
   const navigate = useNavigate();
-  const [loginOptions, setLoginOptions] = useState([
-    { key: "1", text: "Michael Scott", value: "Michael Scott" },
-    { key: "2", text: "Prison Mike", value: "Prison Mike" }
-  ]);
-  const [selectedUser, setSelectedUser] = useState('');
-  const [showSelectUserMessage, setShowSelectUserMessage] = useState(false);
-  const [authenticated, setAuthenticated] = useState(false);
-
-
-  const handleDropdownChange = (event: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
-    setSelectedUser(data.value as string);
-    setShowSelectUserMessage(false);
-  };
-  
-  const handleClick = () => {
-    if (selectedUser) {
-      navigate(pathGenerator.SubjectsList);
-    } else {
-      setShowSelectUserMessage(true); 
-    }
-  };
-
-  const handleAddition = (
-    event: React.SyntheticEvent<HTMLElement>,
-    data: DropdownProps 
-  ) => {
-    const newValue = data.value as string; 
-    setLoginOptions(prevOptions => [
-      ...prevOptions,
-      { key: newValue, text: newValue, value: newValue },
-    ]);
-  };
+  const { user, login, logout, authenticated } = useAuth(); 
 
   const signInWithGoogle = () => {
     const auth = getAuth(app);
@@ -63,14 +28,13 @@ const Login = () => {
                 .then((response) => {
                     localStorage.setItem("token", response.data.token);
                     axiosClient.defaults.headers.common['Authorization'] = `Token ${response.data.token}`;
-                    // Handle response or navigate as needed
-                    navigate(pathGenerator.SubjectsList); // Example navigation
+                    const userName = result.user.displayName || result.user.email || 'Unknown User';
+                    login(userName); 
                 })
                 .catch((error) => {
                     console.error("Error posting the ID token to backend:", error);
                 });
             });
-            setAuthenticated(true);
         }).catch((error) => {
             // Handle Errors here.
             const errorCode = error.code;
@@ -80,46 +44,37 @@ const Login = () => {
   };
   const handleLogout = () => {
     axiosClient.defaults.headers.common['Authorization'] = ``;
-    setAuthenticated(false);
+    logout();
   };
   
 
   return (
-    <div style={{ color: colors.darkblue }}>
-      <div style={{ fontSize: titleFontSize, margin: '24px 20px' }}>Login</div>
-        <div style={{ margin: '0 20px 24px 20px' }}>
-          <Dropdown style={{ width: 400, color: colors.darkblue}}
-            placeholder='Select User'
-            fluid
-            search
-            selection
-            allowAdditions
-            hideAdditions
-            onAddItem={handleAddition}
-            onChange={handleDropdownChange} 
-            options={loginOptions}
-            value={selectedUser} 
-          />
-        </div>
-        
-        {showSelectUserMessage && <p style={{ color: colors.orange, position: 'absolute', margin: '0 20px',}}>Please select a user to proceed.</p>}
-      <div style={{ display: 'flex', flexDirection: 'row', margin: '0 345px' }}>
-      
-        <Button onClick={handleClick} style={{backgroundColor: colors.darkblue, color: colors.white }}>Login</Button>
-      </div>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '0 20px' }}>
-        {/* Existing login button and other components */}
-
-        {/* Google sign-in button */}
-        {(<Button onClick={signInWithGoogle} style={{ backgroundColor: '#5541A9', color: colors.white, marginTop: '10px' }}>
-          Sign in with Google
-        </Button>)}
-        {/* {authenticated && (<Button onClick={handleLogout} style={{ backgroundColor: '#5541A9', color: colors.white, marginTop: '10px' }}>
-          Logout
-        </Button>)} */}
-        
-
-        {showSelectUserMessage && <p style={{ color: colors.orange, position: 'relative', marginTop: '10px'}}>Please select a user to proceed.</p>}
+    <div style={{ color: colors.blue, fontFamily: 'Arial, sans-serif', padding: '20px' }}>
+      <h1 style={{ fontSize: '24px', textAlign: 'center', margin: '20px 0' }}>Login</h1>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '20px',
+        borderRadius: '8px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        backgroundColor: '#FFFFFF'
+      }}>
+        {/* Conditional rendering for Google sign-in/logout button */}
+        {!authenticated ? (
+          <Button color='violet' onClick={signInWithGoogle} style={{ margin: '10px 0' }}>
+            Sign in with Google
+          </Button>
+        ) : (
+          <>
+          <div style={{ margin: '10px 0', fontSize: '16px' }}>
+            Logged as {user} {/* Display the logged-in username */}
+          </div>
+          <Button color='violet' onClick={handleLogout} style={{ margin: '10px 0' }}>
+            Logout
+          </Button>
+        </>
+        )}
       </div>
     </div>
   );
